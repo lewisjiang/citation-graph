c4f8a47d2d364042c1a8566b4d94d227dfbffec7
Bratislava <33171077+lewisjiang@users.noreply.github.com>
2022-04-06 16:51:10 +0800

    better display

--- citation_graph.py
+++ citation_graph.py
@@ -7,13 +7,18 @@ import time
 # Abstract Retrieval, 10,000 per week, 9 per sec.
 
 class CitationGraph:
-    def __init__(self, doi_lst, max_age=30, min_gap=0.1):
+    def __init__(self, doi_lst, ignore_lst=[], max_age=30, min_gap=0.1):
         assert max_age > 7
         self.max_age = max_age
-        self.curr_refs = dict()  # entry: 'id':[Reference, local_id_set]
+        self.curr_refs = dict()  # entry: ref.id: [Reference, local_id_set]
+
         self.doi_list = []
         for doi in doi_lst:
             self.doi_list.append(doi.strip())  # lower creates a new profile, but the online query is case insensitive.
+
+        self.ignored_refs = set()  # a set of scopus_id strings that we wish to block
+        for i in ignore_lst:
+            self.ignored_refs.add(i.strip())
 
         print("A total %d input dois." % len(doi_lst))
 
@@ -41,66 +46,70 @@ class CitationGraph:
 
         dat = [(i, j,) for i, j in enumerate(self.v_full) if j]
 
-        print("\n" + "#" * 32, "Query papers (Input order)")
-        print(fmt % tuple(tab_head))
+        for case in range(2):
+            if case == 0:
 
-        for itm in dat:
-            print(fmt % (str(itm[0])[:col_gap[0]],
-                         str(itm[1].citedby_count)[:col_gap[1]],
-                         str(itm[1].title)[:col_gap[2]],
-                         str(itm[1].coverDate)[:col_gap[3]],
-                         str(itm[1].sourcetitle_abbreviation)[:col_gap[4]],
-                         "" if len(itm[1].authors) < 1 else str(itm[1].authors[-1].indexed_name)[:col_gap[5]],
-                         "" if len(itm[1].affiliation) < 1 else str(itm[1].affiliation[0].name)[:col_gap[6]],
-                         itm[1].doi[:col_gap[7]] if itm[1].doi else ""
-                         ))
+                print("\n" + "#" * 32, "Query papers (Input order)")
+                print(fmt % tuple(tab_head))
 
-        print("\n" + "#" * 32, "Query papers (Most cited)")
-        print(fmt % tuple(tab_head))
+            elif case == 1:
 
-        dat.sort(key=lambda ent: int(ent[1].citedby_count) if ent[1].citedby_count else 0, reverse=True)
+                print("\n" + "#" * 32, "Query papers (Most cited)")
+                print(fmt % tuple(tab_head))
 
-        for itm in dat:
-            print(fmt % (str(itm[0])[:col_gap[0]],
-                         str(itm[1].citedby_count)[:col_gap[1]],
-                         str(itm[1].title)[:col_gap[2]],
-                         str(itm[1].coverDate)[:col_gap[3]],
-                         str(itm[1].sourcetitle_abbreviation)[:col_gap[4]],
-                         "" if len(itm[1].authors) < 1 else str(itm[1].authors[-1].indexed_name)[:col_gap[5]],
-                         "" if len(itm[1].affiliation) < 1 else str(itm[1].affiliation[0].name)[:col_gap[6]],
-                         itm[1].doi[:col_gap[7]] if itm[1].doi else ""
-                         ))
+                dat.sort(key=lambda ent: int(ent[1].citedby_count) if ent[1].citedby_count else 0, reverse=True)
+            else:
+                continue
+
+            for itm in dat:
+                print(fmt % (str(" %2dr" % itm[0])[:col_gap[0]],
+                             str(itm[1].citedby_count)[:col_gap[1]],
+                             str(itm[1].title)[:col_gap[2]],
+                             str(itm[1].coverDate)[:col_gap[3]],
+                             str(itm[1].sourcetitle_abbreviation)[:col_gap[4]],
+                             "" if len(itm[1].authors) < 1 else str(itm[1].authors[-1].indexed_name)[:col_gap[5]],
+                             "" if len(itm[1].affiliation) < 1 else str(itm[1].affiliation[0].name)[:col_gap[6]],
+                             itm[1].doi[:col_gap[7]] if itm[1].doi else ""
+                             ))
 
     def print_refs(self):
-        tab_head = ["#", "local cites", "total cites", " " * 50 + "title", "cover date",
-                    " " * 30 + "source title", ]
+        num_ignored = 0
+
+        tab_head = ["#", "local cites", "total cites", " " * 50 + "title", "pub year",
+                    " " * 30 + "source title", " " * 5 + "scopus_id"]
         col_gap = []
         fmt = ""
         for hd in tab_head:
             col_gap.append(4 * (len(hd) // 4 + 1))
             fmt += "%%%ds | " % col_gap[-1]
 
-        print("#" * 32, "Cited papers")
+        print("\n" + "#" * 32, "Cited papers")
         print(fmt % tuple(tab_head))
 
         dat = []
         for itm in self.curr_refs.items():
-            if itm:
+            if itm[1]:
                 dat.append(itm[1])
+                if itm[1][0].id and itm[1][0].id in self.ignored_refs:
+                    num_ignored += 1
 
-        dat.sort(key=lambda x: (len(x[1]) if x[1] else 0, int(x[0].citedbycount) if x[0].citedbycount else 0,),
+        dat.sort(key=lambda x: (-len(x[1]) if x[0].id in self.ignored_refs else len(x[1]),
+                                int(x[0].citedbycount) if x[0].citedbycount else 0,),
                  reverse=True)
 
         for i, itm in enumerate(dat):
+            if i == len(dat) - num_ignored:
+                print("-" * 32, "Ignored references:")
             print(fmt % (str(i + 1)[:col_gap[0]],
                          str(len(itm[1]))[:col_gap[1]],
-                         str(itm[0].citedbycount)[:col_gap[2]],
+                         str(itm[0].citedbycount or '-')[:col_gap[2]],
                          str(itm[0].title)[:col_gap[3]],
-                         str(itm[0].publicationyear)[:col_gap[4]],
+                         str(itm[0].publicationyear[:4] if itm[0].publicationyear else "-")[:col_gap[4]],
                          str(itm[0].sourcetitle)[:col_gap[5]],
+                         str(itm[0].id)[:col_gap[6]],
                          ), end='\t')
             for j in sorted(list(itm[1])):
-                print("%2d" % j, end=",")
+                print(" %2dr" % j, end=",")
             print()
 
     def get_bibliography_info(self):
@@ -192,7 +201,7 @@ if __name__ == "__main__":
     #     "10.1109/TPAMI.2017.2658577",
     # ]
 
-    # sparsification
+    # 1. sparsification
     dois = [
         "10.1109/IROS.2016.7759502",
         "10.1109/ICRA.2019.8793836",
@@ -206,10 +215,13 @@ if __name__ == "__main__":
         "10.1109/ICRA.2013.6630556",
         "10.1109/ECMR.2013.6698835",
     ]
-    cg = CitationGraph(dois)
+    ignored = ["84871676827",
+               "58249138093",
+               "84856742278",
+               ]
+
+    cg = CitationGraph(dois, ignored)
 
     cg.get_bibliography_info()
     cg.print_curr_papers()
-
-    print()
     cg.print_refs()
